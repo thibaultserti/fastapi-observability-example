76da0dc58afa77163e804d7e6b933fe5340811bb
Thibault Ayanides <thibault.ayanides@skale-5.com>
2023-09-02 20:31:00 +0200

    fix: fix profiling and python version

--- src/app/constants.py
+++ src/app/constants.py
@@ -1,9 +1,9 @@
 import os
 
-APP_NAME = os.environ.get("APP_NAME", "app-a")
+APP_NAME = os.environ.get("APP_NAME", "fastapi-observability-example-a")
 EXPOSE_PORT = os.environ.get("EXPOSE_PORT", 8000)
-OTLP_GRPC_ENDPOINT = os.environ.get("OTLP_GRPC_ENDPOINT", "http://tempo.ayanides.cloud")
-PYROSCOPE_ENDPOINT = os.environ.get("PYROSCOPE_ENDPOINT", "http://pyroscope.ayanides.cloud")
+OTLP_GRPC_ENDPOINT = os.environ.get("OTLP_GRPC_ENDPOINT", "http://localhost:4317")
+PYROSCOPE_ENDPOINT = os.environ.get("PYROSCOPE_ENDPOINT", "http://localhost:4040")
 
 TARGET_ONE_HOST = os.environ.get("TARGET_ONE_HOST", "app-b")
 TARGET_TWO_HOST = os.environ.get("TARGET_TWO_HOST", "app-c")
