706d1a0a31e7c989613661a6269e2c14a47c7cf5
Thibault Ayanides <thibault.ayanides@skale-5.com>
2023-11-12 22:09:28 +0100

    feat: add profiling

--- src/app/constants.py
+++ src/app/constants.py
@@ -6,8 +6,13 @@ APP_PORT = os.environ.get("APP_PORT", 8000)
 REDIS_HOST = os.environ.get("REDIS_HOST", "")
 REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
 
-ENABLE_TRACING = os.environ.get("ENABLE_TRACING", "True").lower() == "true"
 ENABLE_METRICS = os.environ.get("ENABLE_METRICS", "True").lower() == "true"
 
+ENABLE_TRACING = os.environ.get("ENABLE_TRACING", "True").lower() == "true"
 OTEL_EXPORTER_OTLP_ENDPOINT = os.environ.get("OTEL_EXPORTER_OTLP_ENDPOINT", "otel-collector.monitoring:4318")
 OTEL_SERVICE_NAME = os.environ.get("OTEL_SERVICE_NAME", "fastapi-observability-example")
+
+
+ENABLE_PROFILING = os.environ.get("ENABLE_PROFILING", "True").lower() == "true"
+PYROSCOPE_ENDPOINT = os.environ.get("PYROSCOPE_ENDPOINT", "http://pyroscope.monitoring:4040")
+APP_NAME = os.environ.get("APP_NAME", "fastapi-observability-example")
--- src/app/main.py
+++ src/app/main.py
@@ -2,6 +2,7 @@ import logging
 import random
 
 import httpx
+import pyroscope
 import uvicorn
 
 from constants import (
@@ -9,24 +10,25 @@ from constants import (
     APP_PORT,
     ENABLE_METRICS,
     ENABLE_TRACING,
+    ENABLE_PROFILING,
+    PYROSCOPE_ENDPOINT,
+    APP_NAME,
     OTEL_EXPORTER_OTLP_ENDPOINT,
     OTEL_SERVICE_NAME,
     REDIS_HOST,
     REDIS_PORT,
 )
-from fastapi import FastAPI, HTTPException, status, Request
+from fastapi import FastAPI, HTTPException, Request, status
 from fastapi.responses import JSONResponse
 from opentelemetry import trace
-from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.instrumentation.redis import RedisInstrumentor
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
 from opentelemetry.trace.status import Status, StatusCode
-
-
 from prometheus_fastapi_instrumentator import Instrumentator
 from redis import Redis
 
@@ -42,6 +44,18 @@ logger.setLevel(logging.INFO)
 if ENABLE_METRICS:
     Instrumentator().instrument(app).expose(app, include_in_schema=False)
 
+if ENABLE_PROFILING:
+    pyroscope.configure(
+        application_name    = APP_NAME,
+        server_address      = PYROSCOPE_ENDPOINT,
+        sample_rate         = 100,
+        detect_subprocesses = False,
+        oncpu               = False,
+        gil_only            = False,
+        tags                = {
+        }
+    )
+
 if ENABLE_TRACING:
     resource = Resource(attributes={
         SERVICE_NAME: OTEL_SERVICE_NAME
@@ -56,56 +70,61 @@ if ENABLE_TRACING:
 
 @app.get("/", include_in_schema=False)
 async def health_check():
-    return JSONResponse(content={"status": "OK"})
+    with pyroscope.tag_wrapper({ "endpoint": "/" }):
+        return JSONResponse(content={"status": "OK"})
 
 @app.get("/read/{key}")
 async def read_from_redis(request: Request, key: str):
-    context = TraceContextTextMapPropagator().extract(carrier=request.headers, context=context)
-    with tracer_provider.get_tracer(__name__).start_as_current_span("read_from_redis"):
-        value = redis.get(key)
-        if value is not None:
-            return JSONResponse(content={"key": key, "value": value.decode("utf-8")})
-        else:
-            raise HTTPException(status_code=404, detail="Key not found in Redis")
+    context = TraceContextTextMapPropagator().extract(carrier=request.headers)
+    with tracer_provider.get_tracer(__name__).start_as_current_span("read_from_redis", context=context):
+        with pyroscope.tag_wrapper({ "endpoint": "/read" }):
+            value = redis.get(key)
+            if value is not None:
+                return JSONResponse(content={"key": key, "value": value.decode("utf-8")})
+            else:
+                raise HTTPException(status_code=404, detail="Key not found in Redis")
 
 @app.post("/write/{key}")
 async def write_to_redis(request: Request, key: str, value: str, expiration: int):
-    context = TraceContextTextMapPropagator().extract(carrier=request.headers, context=context)
-    with tracer_provider.get_tracer(__name__).start_as_current_span("write_to_redis"):
-        redis.setex(key, expiration, value)
-        return JSONResponse(content={"message": "Data written to Redis"})
+    context = TraceContextTextMapPropagator().extract(carrier=request.headers)
+    with tracer_provider.get_tracer(__name__).start_as_current_span("write_to_redis", context=context):
+        with pyroscope.tag_wrapper({ "endpoint": "/write" }):
+            redis.setex(key, expiration, value)
+            return JSONResponse(content={"message": "Data written to Redis"})
 
 @app.get("/call")
 async def call_external_api(request: Request):
     context = TraceContextTextMapPropagator().extract(carrier=request.headers)
     with tracer_provider.get_tracer(__name__).start_as_current_span("call_external_api", context=context):
-        async with httpx.AsyncClient() as client:
-            response = await client.get("https://restcountries.com/v3.1/name/france?fullText=true")
-            return JSONResponse(content={"status_code": response.status_code, "content": response.text})
+        with pyroscope.tag_wrapper({ "endpoint": "/call" }):
+            async with httpx.AsyncClient() as client:
+                response = await client.get("https://restcountries.com/v3.1/name/france?fullText=true")
+                return JSONResponse(content={"status_code": response.status_code, "content": response.text})
 
 
 @app.get("/exception")
 async def exception(request: Request):
     context = TraceContextTextMapPropagator().extract(carrier=request.headers)
     with tracer_provider.get_tracer(__name__).start_as_current_span("exception", context=context):
-        try:
-            raise ValueError("sadness")
-        except Exception as ex:
-            logger.error(ex, exc_info=True)
-            span = trace.get_current_span()
+        with pyroscope.tag_wrapper({ "endpoint": "/exception" }):
+            try:
+                raise ValueError("sadness")
+            except Exception as ex:
+                logger.error(ex, exc_info=True)
+                span = trace.get_current_span()
 
-            # generate random number
-            seconds = random.uniform(0, 30)
+                # generate random number
+                seconds = random.uniform(0, 30)
 
-            # record_exception converts the exception into a span event.
-            exception = IOError("Failed at " + str(seconds))
-            span.record_exception(exception)
-            span.set_attributes({'est': True})
-            # Update the span status to failed.
-            span.set_status(Status(StatusCode.ERROR, "internal error"))
-            raise HTTPException(
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                detail=f"Got sadness")
+                # record_exception converts the exception into a span event.
+                exception = IOError("Failed at " + str(seconds))
+                span.record_exception(exception)
+                span.set_attributes({'error': True})
+                # Update the span status to failed.
+                span.set_status(Status(StatusCode.ERROR, "internal error"))
+                raise HTTPException(
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail=f"Error")
 
 if __name__ == "__main__":
     uvicorn.run(app, host=APP_HOST, port=APP_PORT)
